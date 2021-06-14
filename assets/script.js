7d7a504739d05a8449be78b3684122ebe9fa7a88
hotsoup42 <jose22gw@gmail.com>
2021-06-14 15:09:58 -0700

    using vanilla js, but going to use jquery. from now on. i find it eaiser to use for me at the moment.
    created jquery src, with a CDN.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,19 @@
 var APIKey='&appid=0a3eb332892c14227c97ed8390936741';
 var city = document.getElementById('searchTerm').value;
 
-
+// const city = $("<h4>").addClass("card-title").text(response.name);
+// var name = element.textContent;s
 var searchButton = document.getElementById("search-button");
 searchButton.addEventListener("click", getApi);
 
 
 
 //
-var repoList = document.querySelector('ul');
+
  
-//
+//search city
+var cityList = document.querySelector('ul');
+
 function getApi() {
 var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + APIKey;
 //
@@ -19,17 +22,19 @@ fetch(queryURL)
   
   console.log(response)
 
-  console.log(response.name)
+  console.log(response.city)
     return response;
   })
 
 
   .then(function (data) {
-
+    //
+    
+   var cityList = document.querySelector('ul');
     for (var i = 0; i < data.length; i++) {
       var listItem = document.createElement('li');
-      listItem.textContent = data[i].html_url;
-      repoList.appendChild(listItem);
+      listItem.textContent = data[i].name;
+      cityList.appendChild(listItem);
     }
   });
 
