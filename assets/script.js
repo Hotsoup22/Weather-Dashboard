f8629d448c798e5e69d6c8337a8570543484c86b
hotsoup42 <jose22gw@gmail.com>
2021-06-15 16:43:15 -0700

    api call is now working, next is displaying it.
     note:my local harddrive was full, couldnt load browser content, bought a bigger harddrive.

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,58 @@
-var APIKey='&appid=0a3eb332892c14227c97ed8390936741';
-var city = document.getElementById('searchTerm').value;
-
+var APIKey = "&appid=0a3eb332892c14227c97ed8390936741";
+var  userCity = $("#searchTerm").val();
+$("#searchTerm").val(""); 
+var cityList =$(".cityList");
 // const city = $("<h4>").addClass("card-title").text(response.name);
 // var name = element.textContent;s
-var searchButton = document.getElementById("search-button");
-searchButton.addEventListener("click", getApi);
 
+var searchCity = $("#searchCity");
+// searchButton.addEventListener("click", getApi);
+// var searchButton = $.Event( "click", getApi );
 
+console.log('CITY DISPLAY. atleast this works');
 
-//
+var queryURL=
+"https://api.openweathermap.org/data/2.5/weather?q=" + userCity + APIKey;
+searchCity.on("click", cityDisplay);
 
- 
-//search city
-var cityList = document.querySelector('ul');
+  function cityDisplay() {
+   
 
-function getApi() {
-var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + APIKey;
-//
-fetch(queryURL)
-.then(function (response) {
   
-  console.log(response)
+   $.ajax({
+      url: queryURL,
+      method:'GET'
 
-  console.log(response.city)
-    return response;
-  })
-
-
-  .then(function (data) {
-    //
-    
-   var cityList = document.querySelector('ul');
-    for (var i = 0; i < data.length; i++) {
-      var listItem = document.createElement('li');
-      listItem.textContent = data[i].name;
-      cityList.appendChild(listItem);
+     
+    })
+      .then(function (response) {
+        console.log(response)
+        console.log(response.name)
+        console.log(response.weather)
+      });
     }
-  });
-
-  }
+      
+    
   
- 
+  
+
+
+
+
+//   fetch(queryURL)
+//     .then(function (response) {
+//       console.log(response);
+
+//       console.log(response.city);
+//       return response;
+//     })
+
+//     .then(function (data) {
+//       var cityList = document.querySelector("ul");
+//       for (var i = 0; i < data.length; i++) {
+//         var listItem = document.createElement("li");
+//         listItem.textContent = data[i].name;
+//         cityList.appendChild(listItem);
+//       }
+//     });
+// }
