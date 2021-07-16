9d774586f3b37665e64b60f72d1ada3c0dd0a12c
hotsoup42 <jose22gw@gmail.com>
2021-07-16 10:50:58 -0700

    reset index, commentted all old code

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,119 @@
-var APIKey = "&appid=0a3eb332892c14227c97ed8390936741";
-var userCity = $("#searchTerm").val();
-var searchCity = $("#searchCity");
 
-console.log("CITY DISPLAY. atleast this works");
+const searchCity = $("#searchCity").val();
+// $("#searchTerm").val("");  
+console.log(searchCity);
+$("#searchCity").on('keyup', function (e) {
+  if (e.key === 'Enter' || e.keyCode === 13) {
+      console.log("it works");
+      apiCall();
+    
+  }
+});
 
-// $("#searchTerm").keypress(function(event) {
+const APIKey = "&appid=0a3eb332892c14227c97ed8390936741";
+    const queryUrl =
+         "https://api.openweathermap.org/data/2.5/weather?q=" +
+         searchCity +
+         APIKey +
+         "&units=imperial";
+    function apiCall(response){
+      $.ajax({
+        url: queryUrl,
+        method: "GET"
+      })
+      .then(function (response){
+        console.log(response);
+      })
+      return response;
+    }
 
-// 	if (event.keyCode === 13) {
-// 		event.preventDefault();
-// 		searchCity.click();
-// 	}
-// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const APIKey = "&appid=0a3eb332892c14227c97ed8390936741";
+// var userCity = $("#searchTerm").val();
+// var searchCity = $("#searchCity");
+
+// console.log("CITY DISPLAY. atleast this works");
+
+// // $("#searchTerm").keypress(function(event) {
+
+// // 	if (event.keyCode === 13) {
+// // 		event.preventDefault();
+// // 		searchCity.click();
+// // 	}
+// // });
  
 
-function getAPI() {
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    userCity +
-    APIKey +
-    "&units=imperial";
+// function getAPI() {
+//   var queryURL =
+//     "https://api.openweathermap.org/data/2.5/weather?q=" +
+//     userCity +
+//     APIKey +
+//     "&units=imperial";
 
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  })
-    .then(function (response) {
-      console.log(response)
-      return response;
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET",
+//   })
+//     .then(function (response) {
+//       console.log(response)
+//       return response;
    
-    })
+//     })
     
-    .then (function cityInfo(cityData){
+//     .then (function cityInfo(cityData){
      
-      $('.tempLi').text('Temperature: ' + cityData.main.temp + '*F'  );
-      $('.feelsLike').text('Feels like: ' + cityData.main.feels_like + '*F');
-      $('.tempMax').text('Max Temp: ' + cityData.main.temp_max + '*F');
-      $('.tempMin').text('Min Temp: ' + cityData.main.temp_min + '*F');
-      $('.humidity').text('Humidity: ' + cityData.main.humidity );
-      $('.pressure').text('Pressure: ' + cityData.main.pressure );
+//       $('.tempLi').text('Temperature: ' + cityData.main.temp + '*F'  );
+//       $('.feelsLike').text('Feels like: ' + cityData.main.feels_like + '*F');
+//       $('.tempMax').text('Max Temp: ' + cityData.main.temp_max + '*F');
+//       $('.tempMin').text('Min Temp: ' + cityData.main.temp_min + '*F');
+//       $('.humidity').text('Humidity: ' + cityData.main.humidity );
+//       $('.pressure').text('Pressure: ' + cityData.main.pressure );
   
     
-     })
-     .then (function cityTitle(cityData) {
-      $(".card-title").text(userCity);
-      $(".card-body").append(cityData.name);
+//      })
+//      .then (function cityTitle(cityData) {
+//       $(".card-title").text(userCity);
+//       $(".card-body").append(cityData.name);
     
-  })
-}
+//   })
+// }
 
 
 
   
-function tempMain(temp){
-    //   let temp = $("<li>").addClass("list-group-item").text(city);
-    // $(".list").append(listItem);
-}
+// function tempMain(temp){
+//     //   let temp = $("<li>").addClass("list-group-item").text(city);
+//     // $(".list").append(listItem);
+// }
 
-searchCity.one("click", getAPI);
-// console.log(response)
+// searchCity.one("click", getAPI);
+// // console.log(response)
 
 // cityTitle(response);
 // cityInfo(response);
