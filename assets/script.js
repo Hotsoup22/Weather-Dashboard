8f932bb1362a062f96eb079ebc8cc9f02e71e6d0
hotsoup42 <jose22gw@gmail.com>
2021-08-18 14:59:58 -0700

    uvi rateing now shows up with color on text

--- assets/script.js
+++ assets/script.js
@@ -1,148 +1,160 @@
 
-const searchCity = $("#searchCity").val();
-// $("#searchTerm").val("");  
-console.log(searchCity);
-$("#searchCity").on('keyup', function (e) {
-  if (e.key === 'Enter' || e.keyCode === 13) {
-      console.log("it works");
-      apiCall();
-    
+var searchCityBtn = document.querySelector(".cityBtn");
+var city = document.querySelector("#city");
+var cityTitle = document.querySelector(".cityTitle");
+weatherCard = document.querySelector("weather");
+var url = "https://api.openweathermap.org/data/2.5/";
+const APIKey = "0a3eb332892c14227c97ed8390936741";
+
+function setCityStorage(cityKey) {
+  cityKey = localStorage.setItem("cityKey", userCity);
+  cityKey = localStorage.getItem("cityKey");
+  console.log("city key logged ", cityKey);
+  return cityKey;
+}
+
+var getCity = function (event) {
+  event.preventDefault();
+  console.log("getcity");
+
+  userCity = city.value.trim();
+
+  if (userCity) {
+    setCityStorage();
+    getWeather(userCity);
+
+  } else {
+    alert("Please enter a City");
   }
-});
+};
 
-const APIKey = "&appid=0a3eb332892c14227c97ed8390936741";
-    const queryUrl =
-         "https://api.openweathermap.org/data/2.5/weather?q=" +
-         searchCity +
-         APIKey +
-         "&units=imperial";
-    function apiCall(response){
-      $.ajax({
-        url: queryUrl,
-        method: "GET"
-      })
-      .then(function (response){
-        console.log(response);
-      })
-      return response;
-    }
+var getWeather = function (userCity) {
+  //var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+  
 
+  const queryUrl =
+    url  +
+    "weather?q="+
+    userCity +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
+  fetch(queryUrl)
+    .then(function (response) {
+      if (response.ok) {
+        // console.log(response);
+        response.json().then(function (data) {
+          // console.log(data);
+          displayWeather(data, city);
+          var cityLat = `lat=${data.coord.lat}`;
+          var cityLon = `lon=${data.coord.lon}`;
+          console.log(cityLat, cityLon);
+          searchApiByCoordinates(cityLat, cityLon);
+        });
+        
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to GitHub");
+    });
+};
 
+ function searchApiByCoordinates(lat, lon) {
+   console.log("seachApibyCoord")
+   var locQueryUrl = `${url}onecall?${lat}&${lon}&exclude=minutely,hourly&units=imperial&appid=${APIKey}`;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const APIKey = "&appid=0a3eb332892c14227c97ed8390936741";
-// var userCity = $("#searchTerm").val();
-// var searchCity = $("#searchCity");
-
-// console.log("CITY DISPLAY. atleast this works");
-
-// // $("#searchTerm").keypress(function(event) {
-
-// // 	if (event.keyCode === 13) {
-// // 		event.preventDefault();
-// // 		searchCity.click();
-// // 	}
-// // });
- 
-
-// function getAPI() {
-//   var queryURL =
-//     "https://api.openweathermap.org/data/2.5/weather?q=" +
-//     userCity +
-//     APIKey +
-//     "&units=imperial";
-
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET",
-//   })
-//     .then(function (response) {
-//       console.log(response)
-//       return response;
+   fetch(locQueryUrl)
    
-//     })
+       .then(function (response) {
+          if(response.ok) {
+             console.log(" found")
+             response.json().then(function (data) {
+             console.log(data)
+             
+             var uvi = data.daily[0].uvi
+             cityTitle.textContent = userCity;
+
+             const resultCard = document.createElement("div");
+             resultCard.classList.add("card", "bg-light", "text-dark", "mb-3", "p-3");
+           
+             const resultBody = document.createElement("div");
+             resultBody.classList.add("card-body");
+             resultCard.append(resultBody);
+           
+             const titleEl = document.createElement("h3");
+             titleEl.textContent = `City: ${userCity}`;
+           
+             const bodyContentEl = document.createElement("p");
+             bodyContentEl.innerHTML = `Uvi Rating: `+  `${uvRating(uvi)}F<br>`
+             resultBody.append(titleEl, bodyContentEl);
+             forecastBody.append(resultCard);
+             
+          }); 
+        } else {
+        alert("Error: " + response.statusText);
+      }
+        })
+        .catch(function (error) {
+          alert("Unable to connect to GitHub");
+        });
+    };
     
-//     .then (function cityInfo(cityData){
-     
-//       $('.tempLi').text('Temperature: ' + cityData.main.temp + '*F'  );
-//       $('.feelsLike').text('Feels like: ' + cityData.main.feels_like + '*F');
-//       $('.tempMax').text('Max Temp: ' + cityData.main.temp_max + '*F');
-//       $('.tempMin').text('Min Temp: ' + cityData.main.temp_min + '*F');
-//       $('.humidity').text('Humidity: ' + cityData.main.humidity );
-//       $('.pressure').text('Pressure: ' + cityData.main.pressure );
-  
-    
-//      })
-//      .then (function cityTitle(cityData) {
-//       $(".card-title").text(userCity);
-//       $(".card-body").append(cityData.name);
-    
-//   })
-// }
 
+function uvRating(uvi) {
 
+  if(uvi < 3 ){
+   return uvi+ ": Low".fontcolor("green")
+  } else if (uvi < 6){
+    returnuvi+ " Moderate" .fontcolor("yellow")
+  } else if(uvi < 8){
+    return uvi+" High".fontcolor("red")
+  }else if (uvi < 11){
+    return uvi+" Very High".fontcolor("black")
+  } else 
 
-  
-// function tempMain(temp){
-//     //   let temp = $("<li>").addClass("list-group-item").text(city);
-//     // $(".list").append(listItem);
-// }
+  return  "uvExt";
+}
+var div = document.createElement("div");
+var litag = document.createElement("li");
+var cityTitle = document.querySelector(".cityTitle");
+var maintempUl = document.querySelector(".maintemp");
+var resultContentEl = document.querySelector(".mainTempL");
+var forecastBody = document.querySelector(".forecastBody");
 
-// searchCity.one("click", getAPI);
-// // console.log(response)
+//display main
+var displayWeather = function (data, city) {
+  console.log(data);
 
-// cityTitle(response);
-// cityInfo(response);
-// display5Days(response);
+  cityTitle.textContent = userCity;
 
-//   })
+  const resultCard = document.createElement("div");
+  resultCard.classList.add("card", "bg-light", "text-dark", "mb-3", "p-3");
 
-//   var card = $(".card");
-//   var cardBody = $(".card-body");
-//   var cardTitle = $(".card-title");
-//   var listGroup = $(".list-group");
-//   var listGroupItem = $(".list-group-item")
+  const resultBody = document.createElement("div");
+  resultBody.classList.add("card-body");
+  resultCard.append(resultBody);
 
+  const titleEl = document.createElement("h3");
+  titleEl.textContent = `City: ${userCity}`;
 
+  const bodyContentEl = document.createElement("p");
+  bodyContentEl.innerHTML = `Feels like: ${data.main.feels_like}F<br>
+Temp: ${data.main.temp}F<br>
+pressure: ${data.main.pressure}<br>
+humidity ${data.main.humidity}<br>
+temp_max: ${data.main.temp_max}F<br>
+temp_min: ${data.main.temp_min}F<br>
+visibility: ${data.visibility}<br>
+${data.weather[0].main}<br>
+${data.weather[0].description}<br>`;
+// ${uvRating(data.current.uvi)}<br>
+  resultBody.append(titleEl, bodyContentEl);
 
-// function display5Days(forecast){
+  resultContentEl.append(resultCard);
+};
+//use the one call api on open weather dashboard
 
-//   var forecastCard = $('.forecastCard');
-//   var forecastTitle = $('.forecastTitle');
-//   var forecastUL = $('.forecastUl');
+searchCityBtn.addEventListener("click", getCity);
 
-//   var forecastDate = document.createElement("h5")
-//   forecastTitle.textContent = "5-Day Forecast:";
-//   // console.log (cityData+'this is 02');
-//   // var forecast = response.weather;
-//   console.log (forecast.weather+'this is it OGS');
-//       for(var i=5; i < forecast.length; i=i+8){
-
-//      var dailyForecast = forecast[i];
-//         console.log (dailyForecast+'this is it');
-//       }
-//     }
